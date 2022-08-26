b7bc3497810d405caea951b42a0cfce3790fcf0e
avb56 <alexeyburtasov@gmail.com>
2022-08-26 18:02:39 +0500

    form4

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -12,9 +12,11 @@ exports.services = (req, res) => res.status(200).json(
 
 exports.service1 = (req, res) => res.status(200).json(
   {
-    "type": "onePage",
+    "type": "step",
+    "name": "Сервис 1",
     "forms": [
       {
+        "name": "Шаг 1",
         "schema": {
           "title": "Форма регистрации",
           "description": "",
@@ -98,6 +100,7 @@ exports.service1 = (req, res) => res.status(200).json(
         }
       },
       {
+        "name": "Шаг 2",
         "schema": {
           "title": "Todo",
           "type": "object",
@@ -125,8 +128,10 @@ exports.service1 = (req, res) => res.status(200).json(
 exports.service2 = (req, res) => res.status(200).json(
   {
     "type": "onePage",
+    "name": "Сервис 2",
     "forms": [
       {
+        "name": "Шаг 1",
         "schema": {
           "title": "Todo",
           "type": "object",
@@ -145,6 +150,90 @@ exports.service2 = (req, res) => res.status(200).json(
               "default": false
             }
           }
+        }
+      },
+      {
+        "name": "Шаг 2",
+        "schema": {
+          "title": "Форма регистрации",
+          "description": "",
+          "type": "object",
+          "required": ["firstName", "lastName"],
+          "properties": {
+            "lastName": {
+              "type": "string",
+              "title": "Фамилия "
+            },
+            "firstName": {
+              "type": "string",
+              "title": "Имя"
+            },
+            "Patronymic": {
+              "type": "string",
+              "title": "Отчество"
+            },
+            "age": {
+              "type": "integer",
+              "title": "Возраст"
+            },
+            "gender": {
+              "type": "boolean",
+              "enumNames": ["Мужской", "Женский"],
+              "title": "Ваш пол"
+            },
+            "date": {
+              "type": "string",
+              "title": "Дата рождения"
+            },
+            "document": {
+              "type": "string",
+              "format": "data-url",
+              "title": "Ваш документ"
+            },
+            "password": {
+              "type": "string",
+              "title": "Пароль",
+              "minLength": 3
+            },
+            "telephone": {
+              "type": "string",
+              "title": "Телефон",
+              "minLength": 10
+            }
+          }
+        },
+        "uiSchema": {
+          "firstName": {
+            "ui:autofocus": true,
+            "ui:emptyValue": ""
+          },
+          "age": {
+            "ui:widget": "updown",
+            "ui:title": "Age of person",
+            "ui:description": "(earthian year)"
+          },
+          "gender": {
+            "ui:widget": "radio"
+          },
+          "date": {
+            "ui:widget": "date"
+          },
+          "password": {
+            "ui:widget": "password",
+            "ui:help": "Hint: Make it strong!"
+          },
+          "telephone": {
+            "ui:options": {
+              "inputType": "tel"
+            }
+          }
+        },
+        "formData": {
+          "firstName": "Chuck",
+          "lastName": "",
+          "age": 75,
+          "bio": "Roundhouse kicking asses since 1940",
+          "password": "noneed"
         }
       }
     ]
